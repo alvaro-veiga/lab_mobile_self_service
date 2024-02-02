889b3c392ee38fbeb6924620fb201b8b1ed52543
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-02-02 17:07:56 -0300

    config assets

--- lab_self_service/lib/src/core/env.dart
+++ lab_self_service/lib/src/core/env.dart
@@ -1,4 +1,4 @@
 
 final class Env {
-  static const backendBaseUrl = String.fromEnvironment('BACKEND_BASE_URL', defaultValue: 'http://localhost:8080');
+  static const backendBaseUrl = String.fromEnvironment('BACKEND_BASE_URL');
 }
