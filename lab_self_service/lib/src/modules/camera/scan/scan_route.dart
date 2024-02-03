3113b0e3ae97aba4c098cd5b516d36fed6f9ea49
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-02-03 13:39:37 -0300

    Remove incorrects imports

--- lab_self_service/lib/src/modules/camera/scan/scan_route.dart
+++ lab_self_service/lib/src/modules/camera/scan/scan_route.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_getit/flutter_getit.dart';
-import './scan_controller.dart';
-import './scan_page.dart';
 
 class CameraRoute extends FlutterGetItModulePageRouter {
   @override
