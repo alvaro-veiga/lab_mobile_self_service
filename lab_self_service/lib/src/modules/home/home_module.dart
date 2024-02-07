e510320d12af9c2aa046d928bc6b8089296c1884
Alvaro.Veiga <alvarojoao17@gmail.com>
2024-02-07 18:13:05 -0300

    remove comments

--- lab_self_service/lib/src/modules/home/home_module.dart
+++ lab_self_service/lib/src/modules/home/home_module.dart
@@ -5,11 +5,9 @@ import 'package:lab_self_service/src/modules/home/home_page.dart';
 
 class HomeModule extends FlutterGetItModule{
   @override
-  // TODO: implement moduleRouteName
   String get moduleRouteName => '/home';
 
   @override
-  // TODO: implement pages
   Map<String, WidgetBuilder> get pages => {
     '/': (_) => const HomePage()
   };
--- lab_self_service/lib/src/modules/scan/scan_module.dart
+++ lab_self_service/lib/src/modules/scan/scan_module.dart
@@ -5,11 +5,9 @@ import 'package:lab_self_service/src/modules/scan/scan_page.dart';
 
 class ScanModule extends FlutterGetItModule{
   @override
-  // TODO: implement moduleRouteName
   String get moduleRouteName => '/scan';
 
   @override
-  // TODO: implement pages
   Map<String, WidgetBuilder> get pages => {
     '/': (_) => const ScanPage()
     };
